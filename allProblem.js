9f1bcdc720181bf388de2747978b0364a11cc184
ZUBAER <zubaer.developer@gmail.com>
2025-08-15 09:47:48 +0600

    solve problem 4

--- allProblem.js
+++ allProblem.js
@@ -29,3 +29,19 @@ function bestTeam(player1, player2) {
     return "Tie";
   }
 }
+
+
+function isSame(arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return "Invalid";
+  }
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
--- problem4.js
+++ problem4.js
@@ -2,8 +2,16 @@ function isSame(arr1, arr2) {
   if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
     return "Invalid";
   }
-  return "ok";
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
-const result = isSame([] , []);
+const result = isSame({data: [2 , 5 , 6] } , [2 , 5 , 6]);
 console.log(result);
